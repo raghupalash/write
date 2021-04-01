f244c82851edb8923c6ddd204312ddbd3f1e60aa
Palash Singh Raghuwanshi <singhpalash0@gmail.com>
2021-04-01 14:59:48 +0530

    Change react component file structure

--- write/frontend/src/components/App.js
+++ write/frontend/src/components/App.js
@@ -1,59 +1,12 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
-import SectionForm from './SectionForm/SectionForm';
+import Create from './SectionForm/Create';
 
 
 class App extends Component {
-	constructor(props) {
-		super(props);
-
-		this.state = {
-			sections: [], 
-			heading: '',
-		}
-	}
-
 	render() {
-		return (
-			<div>
-				<div className="container">
-					<h1>{this.state.heading}</h1>
-					<div>
-						{this.state.sections.map(item => {
-							return(
-							<div key={this.state.sections.indexOf(item)}>
-								<p className={item.headingChosen}>{item.heading}</p>
-								<p>{item.paragraph}</p>
-							</div>)
-						})}
-					</div>
-					{this.state.heading !== '' 
-						?
-						<SectionForm dataToApp={this.dataSectionForm} wantText={true}/>
-						:
-						<SectionForm dataToApp={this.dataSectionForm} wantText={false}/>
-					}
-				</div>
-			</div>
-		);
-	}
-
-	dataSectionForm = data => {
-		// Let's see if it's only heading or if it's a section
-		if (data.paragraph === '') {
-			this.setState({
-				heading: data.heading,
-			})
-		} else {
-			let sections = this.state.sections
-			sections.push(data);
-
-			this.setState({
-				sections: sections,
-			})
-			console.log(this.state.sections);
-		}
+		return <Create />
 	}
 }
 
---
+++ write/frontend/src/components/SectionForm/Create.js
@@ -0,0 +1,59 @@
+import React, {Component} from 'react';
+
+import SectionForm from './SectionForm';
+
+
+class Create extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			sections: [], 
+			heading: '',
+		}
+	}
+
+	render() {
+		return (
+			<div>
+				<div className="container">
+					<h1>{this.state.heading}</h1>
+					<div>
+						{this.state.sections.map(item => {
+							return(
+							<div key={this.state.sections.indexOf(item)}>
+								<p className={item.headingChosen}>{item.heading}</p>
+								<p>{item.paragraph}</p>
+							</div>)
+						})}
+					</div>
+					{this.state.heading !== '' 
+						?
+						<SectionForm dataToApp={this.dataSectionForm} wantText={true}/>
+						:
+						<SectionForm dataToApp={this.dataSectionForm} wantText={false}/>
+					}
+				</div>
+			</div>
+		);
+	}
+
+	dataSectionForm = data => {
+		// Let's see if it's only heading or if it's a section
+		if (data.paragraph === '') {
+			this.setState({
+				heading: data.heading,
+			})
+		} else {
+			let sections = this.state.sections
+			sections.push(data);
+
+			this.setState({
+				sections: sections,
+			})
+			console.log(this.state.sections);
+		}
+	}
+}
+
+export default Create;
